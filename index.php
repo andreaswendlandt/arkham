0c5237b89eeaee8ff4350cde4011cd9f8d3ca6a9
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2014-08-31 17:09:06 +0200

    improved

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <?php
 require_once("Holiday.class");
-if(isset($_POST['Destination']))
+if(!isset($_POST['Destination']))
 {
-	if(!isset($_POST['desire']))
-	{
-		header("location: index.php");
-		exit();
-	}
-	else
+        try
+        {
+                $holidaycatalog = new Holiday;
+                $holidaycatalog->connectHost("credentials.php");
+                $holidaycatalog->selectDatabase("urlaub");
+                $holidaycatalog->showContinents();
+        }
+        catch(Exception $exception)
+        {
+                echo $exception->getMessage();
+                exit();
+        }
+}
+else
+{
+	if(isset($_POST['desire']))
 	{
 		try
 		{
@@ -21,21 +31,6 @@ if(isset($_POST['Destination']))
 			echo $exception->getMessage();
 			exit();
 		}
-	}
-}
-else
-{
-	try
-	{
-		$holidaycatalog = new Holiday;
-		$holidaycatalog->connectHost("credentials.php");
-		$holidaycatalog->selectDatabase("urlaub");
-		$holidaycatalog->showContinents();
-	}
-	catch(Exception $exception)
-	{
-		echo $exception->getMessage();
-		exit();
 	}
 }
 ?>
