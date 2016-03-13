5c95ab1fceee9ea03449d9a8e91dc447952930aa
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2016-03-13 18:09:07 +0100

    networkscanner

---
+++ check_ip.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then 
+  echo "Error, this script needs 2 parameters, a network and an ipaddress"
+  echo "usage: $0 <10.1.7.0/24> <10.1.7.8>"
+  exit 1
+fi
+
+network="$1"
+ipaddress="$2"
+
+result=$(nmap -sL $network | grep "$ipaddress\>")
+echo $result
+
+exit 0
+  
+  TCP_TIMEOUT=3
+  while read host port; do
+    (CURPID=$BASHPID;
+    (sleep $TCP_TIMEOUT;kill $CURPID) &
+    exec 3<> /dev/tcp/$host/$port
+    ) 2>/dev/null
+    case $? in
+    0)
+      echo $host $port is open;;
+    1)
+      echo $host $port is closed;;
+    143) # killed by SIGTERM
+       echo $host $port timeouted;;
+     esac
+  done
+  ) 2>/dev/null
