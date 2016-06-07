9eb76a7da421968d8d01d38d26dbc90720cd8d3d
Andreas Wendlandt <andreaswendlandt.79@googlemail.com>
2016-06-07 16:24:51 +0200

    imprived syntax

--- check_bacula.sh
+++ check_bacula.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "status all" | bconsole | awk '$2 ~ /Full/ || $2 ~ /Incr/' | awk '$6 ~ /Error/'
+echo "status all" | bconsole | awk '$2 ~ /Full/ || $2 ~ /Incr/' | awk '$6 ~ /Error/ || $5 ~ /Error/'
