e667cb3ea5b8707d8fff607f1126051f045dff21
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2016-08-18 14:13:47 +0200

    renamed

--- mysql.sh
+++
@@ -1,4 +0,0 @@
-
-SELECT table_name FROM INFORMATION_SCHEMA.TABLES WHERE engine = 'myisam';
-
-echo "SELECT concat('ALTER TABLE ',TABLE_NAME,' ENGINE=InnoDB;') FROM Information_schema.TABLES WHERE ENGINE = 'MyISAM' AND TABLE_TYPE='BASE TABLE'" | mysql --defaults-file=/etc/mysql/debian.cnf >convert.sql
