51c265088d27fc669e464b17b1945682729a51d5
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2016-08-24 12:23:33 +0200

    added scp command with password

--- oneliner.sh
+++ oneliner.sh
@@ -4,3 +4,5 @@ SELECT table_name FROM INFORMATION_SCHEMA.TABLES WHERE engine = 'myisam';
 echo "SELECT concat('ALTER TABLE ',TABLE_NAME,' ENGINE=InnoDB;') FROM Information_schema.TABLES WHERE ENGINE = 'MyISAM' AND TABLE_TYPE='BASE TABLE'" | mysql --defaults-file=/etc/mysql/debian.cnf >convert.sql
  
 find /var/www/pg/import -type f -printf '%f\t%s\t\t%TY.%Tm.%Td\n' | numfmt --field=2 --to=iec-i
+
+for i in $serverlist; do echo $i; sshpass -p <super_strong_password> scp whatever user@${i}:/wherever; done
