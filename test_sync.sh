c5fa9f03181c865e60a14aa53b50976795880ed2
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2016-08-31 13:21:16 +0200

    improved syntax

--- test_sync.sh
+++ test_sync.sh
@@ -1,13 +1,18 @@
-#!/bin/bash
-backup_folder=$1
-user_folder=$2
-if [ -z "$backup_folder" -o -z "$user_folder" ]
-then
-  echo "error, you need to provide 2 parameters"
-  return 1
-else
-  for folder in $(ls -1 $backup_folder | grep -v roaming)
-  do
-    rsync -av ${backup_folder}/$folder ${user_folder}/$folder
-  done
-fi
+folder_sync () 
+{ 
+    backup_folder=$1;
+    user_folder=$2;
+    if [ -z "$backup_folder" -o -z "$user_folder" ]; then
+        echo "error, you need to provide 2 parameters";
+        return 1;
+    else
+        ls -1 $backup_folder | grep -v roaming | while read a; do
+            set $a;
+            if [ $# -eq 2 ]; then
+                cp -a -r ${backup_folder}/$1\ $2 $user_folder;
+            else
+                cp -a -r ${backup_folder}/$@ $user_folder;
+            fi;
+        done;
+    fi
+}
