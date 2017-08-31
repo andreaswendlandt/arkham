42959b2b84ca3ec6e6606aa2cc98f23efc4bf574
Andreas Wendlandt <andreaswendlandt.79@googlemail.com>
2017-08-31 16:45:16 +0200

    put host information (custom host notes) from a wiki to your check_mk

---
+++ check_mk_notes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# author: guerillatux
+# last mofified: 31.8.2017
+# desc: some crazy-fancy-awesome stuff
+
+# download and parse documentation file, put that to the custom notes section in check_mk
+if scp server:/path_to_file /tmp/ >/dev/null; then
+    cat /tmp/file | while read line; do
+        name=$(echo $line | egrep -o 'servername_regex' | uniq)
+        if  [ -n "$name" ]; then
+            touch /path_to_check_mk/${name}
+            txt=$(echo $line | egrep -o  '\<eco-vs-[[:digit:]]*]] .*\\' | sed -e 's/.*]] //' | sed -e 's/\\//')
+            echo "$txt <a href=\"http://your_url/${ame}\">$name at your wiki</a>" >/path_to_check/${name}
+        fi
+    done
+else
+    echo "something went wrong, could not download the necessary file, please check manually!"
+    exit 1
+fi
