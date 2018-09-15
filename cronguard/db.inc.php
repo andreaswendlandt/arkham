7e39c723deca67ece64795e307711b5a7a3daf27
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2018-09-16 00:27:49 +0200

    db.inc.php for connecting to the database

---
+++ cronguard/db.inc.php
@@ -0,0 +1,7 @@
+<?php
+$conn = @(new mysqli("localhost", "cronguard", "egal", "cronguard"));
+if ($conn->connect_error) {
+  echo "Error while connecting: " . mysqli_connect_error() . "<hr />";
+  exit();
+}
+?>
