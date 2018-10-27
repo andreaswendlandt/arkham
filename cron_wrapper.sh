e148fdd6650c8fffb82b00d50b292d6c1fda3bcf
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2018-10-27 23:55:54 +0200

    better token generation (hex value)

--- cron_wrapper.sh
+++ cron_wrapper.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 # author: guerillatux
 # desc: simple wrapper script for cronjobs which have piped commands
-# last modified: 24.6.2018
+# desc: it notifies the cronguard server via curl the start-/endtime and the result of a script 
+# last modified: 12.08.2018
 
-if [ $# -ne 2 ]; then
-    echo "This Script needs 2 Parameters, a Token and a Command-Chain"
-    echo "Usage: $0 <ghgf4vl5> <\"command1|command2|command3\">"
+if [ $# -ne 1 ]; then
+    echo "This Script needs 1 Parameter, a Command-Chain"
+    echo "Usage: $0 <\"command1|command2|command3\">"
     exit 1
 fi
 
-token="$1"
-command="$2"
-url=
+if ! which curl >/dev/null; then
+    echo "Error, curl is missing on this system!"
+    exit 1
+fi
 
-#curl ${url}/${token}\;b
+command="$1"
+host=$(hostname)
+token=$(openssl rand -base64 4 | sed -e 's/==//')
+start_time=$(date +%s)
+action="start"
+
+curl -X POST -F "token=$token" -F "host=$host" -F "start_time=$start_time" -F "command=$command" -F "action=$action "http://localhost/cron.php
+
 eval "$command; "'pipe=${PIPESTATUS[*]}'
 set $pipe
+
 j=1
 error=
 for i in $*; do
@@ -25,9 +35,13 @@ for i in $*; do
     j=$((j+1))
 done
 
+result=
 if [ -z "$error" ]; then
-    #curl ${url}/${token}\;e 
-    :
+    result="success"
 else
-    echo "Error at the following Command(s): $error"
+    result="fail"
 fi
+
+action="finished"
+end_time=$(date +%s)
+curl -X POST -F "token=$token" -F "action=$action" -F "end_time=$end_time" -F "result=$result" http://localhost/cron.php
