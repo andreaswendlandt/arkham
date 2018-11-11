2df182314373000a525f45f037b65268e371f30f
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2018-11-11 22:12:00 +0100

    adding the cronguard functionality

--- cronguard/cronguard.sh
+++ cronguard/cronguard.sh
@@ -15,7 +15,7 @@ pidfile="/var/run/cronguard.pid"
 init_pidfile="/var/run/cronguard_init.pid"
 logfile="/var/log/cronguard.log"
 pid="$$"
-interval=7
+interval=60
 
 # Check if called with sudo (important for checking the processes)
 if  ps -ef | grep $((pid -1)) | grep sudo >/dev/null 2>&1; then 
@@ -121,10 +121,39 @@ check_cronguard() {
 }
 
 # Cronguard Function
-cronguard(){
-    echo "$(date) start" >/home/andreas/testfile
-    sleep 5
-    echo "$(date) stop" >>/home/andreas/testfile
+cronguard() {
+    query=$(mysql -u $db_user -p$db_password $db -e"SELECT token,result,host FROM $table" 2>&1 | grep -v 'Using a password' | tail -n +2)
+    if ! [ -z "$query" ]; then
+        echo -e "$query" | while read line; do
+            set $line
+	    if [ "$2" == "success" ]; then
+                log "$1 will be removed"
+	        if mysql -u $db_user -p$db_password $db -e"DELETE FROM $table WHERE token = \"$1\"" 2>&1 | grep -v 'Using a password'; then
+                    log "$1 from $3 removed"
+                fi
+            elif [ "$2" == "fail" ]; then
+                log "$1 will be sent"
+                failed_command=$(mysql -u $db_user -p$db_password $db -e"SELECT command FROM $table WHERE token =\"$1\"" 2>&1 | grep -v 'Using a password' | tail -n +2)
+                if send_mail "Failed Cronjob on $3" "This Cronjob failed: \"$failed_command\""; then
+	            mysql -u $db_user -p$db_password $db -e"DELETE FROM $table WHERE token = \"$1\"" 2>&1 | grep -v 'Using a password'
+	        fi    
+            elif [ "$2" == "NULL" ]; then
+                log "$1 is still running"
+	        current_time=$(date +%s)
+                start_time=$(mysql -u $db_user -p$db_password $db -e"SELECT start_time FROM $table WHERE token =\"$1\"" 2>&1 | grep -v 'Using a password' | tail -n +2)
+	        running_since=$((current_time-start_time))
+                if [ $running_since -ge 8 ]; then
+                #if [ $running_since -ge 86400 ]; then
+                long_running_command=$(mysql -u $db_user -p$db_password $db -e"SELECT command FROM $table WHERE token =\"$1\"" 2>&1 | grep -v 'Using a password' | tail -n +2)
+                    if send_mail "Long running Cronjob on $3" "This Cronjob is running longer than one day($running_since seconds): \"$long_running_command\""; then
+                        mysql -u $db_user -p$db_password $db -e"DELETE FROM $table WHERE token = \"$1\"" 2>&1 | grep -v 'Using a password'
+	            fi
+                fi
+            fi
+        done
+    else
+        log "empty db"
+    fi
 }
 
 # Loop Function
