9840ebbf10d601d83f7e1e4fb1582fb524001184
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2018-12-30 23:35:43 +0100

    implemented a simple rest api

--- cronguard/cron.php
+++ cronguard/cron.php
@@ -1,5 +1,35 @@
 <?php
 require_once ("db.inc.php");
+
+function api() {
+    global $conn;
+    $sql = "SELECT * FROM job_foo";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $cronjobs = array();
+        while($row = $result->fetch_assoc()) {
+            $cronjobs[] = $row;
+        } 
+    header('Content-Type: application/json');
+    echo json_encode($cronjobs, JSON_PRETTY_PRINT);
+    }
+    else {
+        echo "0 results";
+    }
+    exit();
+}
+
+if (isset ($_GET['method'])) {
+    $method = $_GET['method'];
+    if ("$method" == "api") {
+        api();
+    }
+    else {
+        echo "Invalid Argument ($method), use api<br>";
+	exit();
+    }
+}
+
 if (isset($_POST['action'])) {
     $action = $_POST['action'];
 }
@@ -17,8 +47,16 @@ if ($action == 'start') {
     else {  
         die("no data retrieved\n");
     }
-    $sql = "INSERT INTO jobs_new (token, host, start_time, command, action)
-    VALUES ('$token', '$host', '$start_time', '$command', '$action')";
+    $stmt = $conn->prepare("INSERT INTO job_foo (token, host, start_time, command, action)
+    VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssiss", $token, $host, $start_time, $command, $action);
+    if ($stmt->execute() === TRUE){
+        echo "New record created successfully";
+    }
+    else {
+        echo "Error with creating a new record";
+    }
+    $stmt->close();
 }
 elseif ($action == "finished") {
     if (isset($_POST['token']) && isset($_POST['end_time']) && isset($_POST['result'])) {
@@ -29,16 +67,18 @@ elseif ($action == "finished") {
     else {
         die("no data retrieved\n");
     }
-    $sql = "UPDATE jobs_new SET end_time='$end_time', action='$action', result='$result' WHERE token='$token'";
+    $stmt = $conn->prepare("UPDATE job_foo SET end_time = ?, action = ?, result = ? WHERE token = ?");
+    $stmt->bind_param("isss", $end_time, $action, $result, $token);
+    if ($stmt->execute() === TRUE){
+        echo "Record updated successfully";
+    }
+    else {
+        echo "Error with updating the record";
+    }
+    $stmt->close();
 }
 else {
     die("something messed up");
-}
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 $conn->close();
