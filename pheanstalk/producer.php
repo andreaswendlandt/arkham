14890cece1850c02654bc601b193b3ad3c970c1c
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2019-02-10 21:53:56 +0100

    added array for multiple jobs

--- pheanstalk/producer.php
+++ pheanstalk/producer.php
@@ -1,5 +1,7 @@
 <?php
+
 require 'vendor/autoload.php';
+
 if (posix_getuid() != 0){
     exit("Error: this script must be run as root!\n");
 }
@@ -15,13 +17,19 @@ function write_log($content){
 
 use Pheanstalk\Pheanstalk;
 
-$job = "echo 'hallo mars' >/tmp/mars; sleep 2;php hallo_mars.php";
-
 function put_job($job) {
-    $pheanstalk = new Pheanstalk('127.0.0.1');
+    $pheanstalk = new Pheanstalk('192.168.1.45');
     $pheanstalk
       ->useTube('testtube')
       ->put("$job");
 }
 
-put_job($job);
+$jobs = array("echo 'hallo mars' >/tmp/mars; sleep 2;php hallo_mars.php",
+	     "echo 'hallo welt' >/tmp/welt; sleep 2;php hallo_welt.php",
+	     "echo 'hallo venus' >/tmp/venus; sleep 2;php hallo_venus.php"
+            );
+
+foreach ($jobs AS $job){
+    put_job($job);
+    write_log("$job put to the queue");
+}
