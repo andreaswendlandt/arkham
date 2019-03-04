e465068b031d2d3f12dea040ac2a9e7f054a55b5
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2019-03-04 21:17:58 +0100

    config file for the producer.php script

---
+++ pheanstalk/producer.inc.php
@@ -0,0 +1,7 @@
+<?php
+// ip address of the beanstalkd server
+$beanstalkd_ip = "192.168.1.45";
+// beanstalkd tube to take jobs from
+$tube = "testtube";
+// jobfile to read from
+$jobfile = "/var/www/html/php/jobs.txt";
