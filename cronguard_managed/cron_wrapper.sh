abbb71b42b6495446ab754f6fb4605dd0c364760
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2020-01-12 21:45:20 +0100

    added check_prerequisits function, extended the output ofin case no parameter was given

--- cronguard_managed/cron_wrapper.sh
+++ cronguard_managed/cron_wrapper.sh
@@ -2,11 +2,16 @@
 # author: guerillatux
 # desc: wrapper script for cronjobs
 # desc: it notifies the cronguard server via curl about the start-/endtime and the result of whatever it was given to 
-# last modified: 10.01.2020
+# last modified: 12.01.2020
 
+# Check if something was given to execute
 if [ $# -ne 1 ]; then
-    echo "This Script needs 1 Parameter, a Command-Chain"
-    echo "Usage: $0 <\"command1|command2|command3\">"
+    echo "This Script needs 1 parameter, either a command-chain, a command or a script"
+    echo "Usage: $0 <command1|command2|command3>"
+    echo "or"
+    echo "$0 <command>"
+    echo "or"
+    echo "$0 <script.sh>"
     exit 1
 fi
 
@@ -21,12 +26,21 @@ if ! which curl >/dev/null; then
     curl_not_present=1
 fi
 
-# Include Config File
+# Include config file
 if ! source /opt/cronguard/token.inc.sh 2>/dev/null; then
     echo "Could not include token.inc.sh from /opt/cronguard, the cronjob will be executed but the cronguard server will not contacted"
     echo "Please fix this manually!"
     token_not_present=1
 fi
+
+# Checking the prerequisites for contacting the cronguard server
+check_prerequisites(){
+    if (( $curl_not_present == 0 )) && (( $token_not_present == 0 )); then
+        return 0
+    else
+        return 1
+    fi
+}
 
 ##Fixme
 # code for validating the token
@@ -39,19 +53,19 @@ url="http://localhost/cronguard_managed/cronguard.php"
 start_time=$(date +%s)
 action="start"
 
-if (( $curl_not_present == 0 )) && (( $token_not_present == 0 )); then
-    # First curl, adding a new Database Entry with the Starttime
+if check_prerequisites; then
+    # First curl, adding a new database entry with the starttime
     #curl -X POST -F "token=$token" -F "host=$host" -F "start_time=$start_time" -F "command=$command" -F "action=$action" $url
     echo "execute first curl"
 else
     echo "not execute first curl"
 fi
 
-# Execute the Cron Command and save the Pipestatus in the Variable "pipe"
+# Execute the cron command and save the pipestatus in the variable "pipe"
 eval "$command; "'pipe=${PIPESTATUS[*]}'
 set $pipe
 
-# Checking the Array for Errors
+# Checking the array for errors
 j=1
 error=
 for i in $*; do
@@ -61,7 +75,7 @@ for i in $*; do
     j=$((j+1))
 done
 
-# Defining the Result for the next Curl and Database Entry
+# Defining the result for the next curl and database entry
 result=
 if [ -z "$error" ]; then
     result="success"
@@ -69,7 +83,7 @@ else
     result="fail"
 fi
 
-if (( $curl_not_present == 0 )) && (( $token_not_present == 0 )); then
+if check_prerequisites; then
     # Define the Endtime and make the second Curl, modify the above Database Entry
     action="finished"
     end_time=$(date +%s)
