91c61f30584a1af94d46ffbf22be37871e5395a9
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2020-01-19 21:32:16 +0100

    add validatetoken.php: called via curl and validates a token

---
+++ cronguard_managed/validatetoken.php
@@ -0,0 +1,18 @@
+<?php
+include "validatetoken.class.php";
+
+if (isset ($_POST['token'])){
+    $token_to_check = new ValidateToken($_POST['token']);
+} else {
+    echo "No token provided, aborting";
+    exit();
+}
+
+$output = $token_to_check->{'get_token'}();
+
+$bool = $token_to_check->{'check_token'}();
+if ($bool){
+    echo "valid";
+} else {
+    echo "invalid";
+}
