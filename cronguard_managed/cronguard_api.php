cbe6c1df6608a06affdc2975e226d9caef5bd261
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2020-01-26 22:02:49 +0100

    api functionality with token validation

--- cronguard_managed/cronguard_api.php
+++ cronguard_managed/cronguard_api.php
@@ -1,11 +1,13 @@
 <?php
 require_once ("db.inc.php");
 include "validatetoken.class.php";
+
 function usage(){
     echo "Call this api as follows:";
     echo "<br>";
     echo "http://localhost/cronguard_managed/cronguard_api.php?method=api&token=`your_token`";
 }
+
 function api($token) {
     global $conn;
     $sql = "SELECT * FROM job_test WHERE token = \"$token\"";
@@ -28,9 +30,17 @@ if (isset ($_GET['method'])) {
     $method = $_GET['method'];
     if (isset ($_GET['token'])) {
         $token = $_GET['token'];
+	$token_to_check = new ValidateToken($token);
+	$bool = $token_to_check->{'check_token'}();
+	if (!$bool) {
+	    echo "Error, your token is not valid";
+	    echo "<br>";
+	    exit();
+	}
     }
     else {
         echo "ERROR, you need to provide a token!";
+	echo "<br>";
 	usage();
 	exit();
     }
@@ -45,75 +55,8 @@ if (isset ($_GET['method'])) {
 }
 
 if (empty($_POST)) {
+    echo "You need to provide a method and a valid token";
+    echo "<br>";
     usage();
     exit();
 }
-
-/*if (isset($_POST['action'])) {
-    $action = $_POST['action'];
-}
-else {
-    die("Something went wrong with the data transmission\n");
-}
-
-if ($action == 'start') {
-    if (isset($_POST['ident']) && isset($_POST['token']) && isset($_POST['host']) && isset($_POST['start_time']) && isset($_POST['command'])) {
-        $ident = $_POST['ident'];
-        $token = $_POST['token'];
-        $host = $_POST['host'];
-        $start_time = $_POST['start_time'];
-        $command = $_POST['command'];
-    }
-    else {  
-        die("no data retrieved\n");
-    }
-    $token_to_check_before = new ValidateToken($_POST['token']);
-    var_dump($token_to_check_before);
-    $bool_before = $token_to_check_before->{'check_token'}();
-    var_dump($bool_before);
-    if ($bool_before){
-        $stmt = $conn->prepare("INSERT INTO job_test (ident, token, host, start_time, command, action)
-        VALUES (?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("sssiss", $ident, $token, $host, $start_time, $command, $action);
-        if ($stmt->execute() === TRUE){
-            echo "New record created successfully";
-        }
-        else {
-            echo "Error with creating a new record";
-        }
-        $stmt->close();
-    } else {
-        echo "Your token is not valid, please create a new one!";
-    }
-}
-elseif ($action == "finished") {
-    if (isset($_POST['ident']) && isset($_POST['end_time']) && isset($_POST['result'])) {
-        $ident = $_POST['ident'];
-        $end_time = $_POST['end_time'];
-        $result = $_POST['result'];
-    }
-    else {
-        die("no data retrieved\n");
-    }
-    $token_to_check_after = new ValidateToken($_POST['token']);
-    var_dump($token_to_check_after);
-    $bool_after = $token_to_check_after->{'check_token'}();
-    var_dump($bool_after);
-    if ($bool_after){
-        $stmt = $conn->prepare("UPDATE job_test SET end_time = ?, action = ?, result = ? WHERE ident = ?");
-        $stmt->bind_param("isss", $end_time, $action, $result, $ident);
-        if ($stmt->execute() === TRUE){
-            echo "Record updated successfully";
-        }
-        else {
-            echo "Error with updating the record";
-        }
-        $stmt->close();
-    }
-}
-else {
-    die("something messed up");
-}
-
-$conn->close();
-?>*/
