0cc95881d7905f94ca6fddbcbe49701105f37f3f
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2020-02-03 22:04:18 +0100

    using post variable and better output

--- cronguard_managed/generatetoken.php
+++ cronguard_managed/generatetoken.php
@@ -1,12 +1,24 @@
 <?php
 include "generatetoken.class.php";
 
-$email = "foo@bar.de";
-$gen_token = new GenerateToken($email);
-$token = $gen_token->{'generate_token'}();
-$bool = $gen_token->{'write_to_database'}($token, $email);
-if ($bool){
-    echo "entry created";
+if (!empty($_POST['email'])){
+    $email = $_POST['email'];
 } else {
-    echo "entry not created";
+    echo "No email address provided, aborting";
+    exit();
+}
+
+$gen_token = new GenerateToken($email);
+$bool_val = $gen_token->{'validate_email'}($email);
+if ($bool_val){
+    $token = $gen_token->{'generate_token'}();
+    $bool_gen = $gen_token->{'write_to_database'}($token, $email);
+        if ($bool_gen){
+            echo "email: $email is valid and a new database entry was created\n";
+    	    echo "Your Token is: \"$token\"";
+        } else {
+            echo "Could not create a new database entry, please contact the admin";
+        }
+} else {
+    echo "email: $email is not valid\n";
 }
