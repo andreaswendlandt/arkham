b9a2397a011b3bff41cab25ee21158b212c2fae8
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2020-02-03 22:12:33 +0100

    added form and switched de to en

--- cronguard_managed/remove_token_mail.php
+++ cronguard_managed/remove_token_mail.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html> 
-<html lang="de"> 
+<html lang="en"> 
 <head> 
 <meta charset="utf-8"> 
-<title>Cronguard</title> 
+<title>Remove Token</title> 
 <meta name="viewport" content="width=device-width; initial-scale=1.0"> 
 </head> 
 <body> 
@@ -11,4 +11,10 @@ require_once('navigation.php');
 echo "<div id=\"nav\"></div>";
 ?>
 <div id="content">
-<h2>Remove your Token and Mailaddress</h2> 
+<h2>Remove your Token and Mailaddress</h2>
+<form method="post" action="removetokenmail.php">
+Token <input type="text" name="token" /><br /><br />
+<input type="submit" name="Submit" value="Send Token" />
+</form>
+</body>
+</html>
