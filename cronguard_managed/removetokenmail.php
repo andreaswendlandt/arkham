801911217b5f17d8686f7eab981e7e5b0e5f9e6d
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2020-02-03 22:11:17 +0100

    using post variable, more verbose output

--- cronguard_managed/removetokenmail.php
+++ cronguard_managed/removetokenmail.php
@@ -1,7 +1,13 @@
 <?php
 include "removetokenmail.class.php";
 
-$token = "2cde7d";
+if (!empty($_POST['token'])){
+    $token = $_POST['token'];
+} else {
+    echo "No token provided, aborting!";
+    exit();
+}
+
 $rem_token = new RemoveTokenMail($token);
 
 $token_valid = $rem_token->{'check_token'}();
@@ -9,10 +15,10 @@ $token_valid = $rem_token->{'check_token'}();
 if ($token_valid){
     $bool = $rem_token->{'remove_from_database'}($token);
     if ($bool){
-        echo "token:$token removed\n";
+        echo "Token: $token removed\n";
     } else {
-        echo "could not remove token:$token\n";
+        echo "Could not remove token: $token\n";
     }
 } else {
-    echo "$token does not exist\n";
+    echo "Token: $token does not exist\n";
 }
