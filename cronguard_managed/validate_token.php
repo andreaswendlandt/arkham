95d04a5c24666daef1ea5a9033539462d6a13701
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2020-02-03 22:09:15 +0100

    added form and switched de to en

--- cronguard_managed/validate_token.php
+++ cronguard_managed/validate_token.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html> 
-<html lang="de"> 
+<html lang="en"> 
 <head> 
 <meta charset="utf-8"> 
-<title>Cronguard</title> 
+<title>Validate Token</title> 
 <meta name="viewport" content="width=device-width; initial-scale=1.0"> 
 </head> 
 <body> 
@@ -12,3 +12,9 @@ echo "<div id=\"nav\"></div>";
 ?>
 <div id="content">
 <h2>Validate a Token</h2> 
+<form method="post" action="validatetoken.php">
+Token <input type="text" name="token" /><br /><br />
+<input type="submit" name="Submit" value="Send Token" />
+</form>
+</body>
+</html>
