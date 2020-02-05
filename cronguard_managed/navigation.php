561b55856d972bc1eb07d37c7b530726f0fd36ae
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2020-02-05 22:27:51 +0100

    new forgot token section

--- cronguard_managed/navigation.php
+++ cronguard_managed/navigation.php
@@ -1,7 +1,8 @@
 <div>
-        <a class="nav" href="index.php">Home</a> 
+	<a class="nav" href="index.php">Home</a>
         <a class="nav" href="generate_token.php">Generate Token</a> 
         <a class="nav" href="validate_token.php">Validate Token</a> 
         <a class="nav" href="download_wrapper.php">Download Wrapper</a> 
         <a class="nav" href="remove_token_mail.php">Remove Token/Mailaddress</a> 
+        <a class="nav" href="forgot_token.php">Forgot Token</a> 
 </div>
