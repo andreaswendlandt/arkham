445ea3e1f3581cab027c31d9b67f41c41def8237
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2020-02-09 21:41:07 +0100

    refactoring the forgot token section- using one script

--- cronguard_managed/forgot_token.php
+++ cronguard_managed/forgot_token.php
@@ -12,9 +12,29 @@ echo "<div id=\"nav\"></div>";
 ?>
 <div id="content">
 <h2>Forgot Token</h2> 
-<form method="post" action="forgottoken.php">
+<form method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
 Email <input type="text" name="email" /><br /><br />
-<input type="submit" name="Submit" value="Send email" />
+<input type="submit" name="submit" value="Send email" />
 </form>
+<br />
+<?php
+if(isset($_POST['submit'])){
+    include "forgottoken.class.php";
+    if (!empty($_POST['email'])){
+        $email = $_POST['email'];
+    } else {
+        echo "No emailaddress provided, aborting";
+        exit();
+    }
+    $email_to_check = new ForgotToken($email);
+    $bool = $email_to_check->{'check_email'}();
+    if ($bool){
+        $token = $email_to_check->{'return_token'}();
+        echo "The token for the emailaddress $email is \"$token\"";
+    } else {
+        echo "Email: $email does not exist";
+    }
+}
+?>
 </body>
 </html>
