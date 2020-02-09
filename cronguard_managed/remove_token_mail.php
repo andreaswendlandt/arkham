c43369832870ef84a7a471fe69b7202d70fee04f
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2020-02-09 21:21:55 +0100

    refactoring the token removing - using one script

--- cronguard_managed/remove_token_mail.php
+++ cronguard_managed/remove_token_mail.php
@@ -12,9 +12,33 @@ echo "<div id=\"nav\"></div>";
 ?>
 <div id="content">
 <h2>Remove your Token and Mailaddress</h2>
-<form method="post" action="removetokenmail.php">
+<form method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
 Token <input type="text" name="token" /><br /><br />
-<input type="submit" name="Submit" value="Send Token" />
+<input type="submit" name="submit" value="Send Token" />
 </form>
+<br />
+<?php
+if(isset($_POST['submit'])){
+    include "removetokenmail.class.php";
+    if (!empty($_POST['token'])){
+        $token = $_POST['token'];
+    } else {
+        echo "No token provided, aborting!";
+        exit();
+    }
+    $rem_token = new RemoveTokenMail($token);
+    $token_valid = $rem_token->{'check_token'}();
+    if ($token_valid){
+        $bool = $rem_token->{'remove_from_database'}($token);
+        if ($bool){
+            echo "Token: $token removed\n";
+        } else {
+            echo "Could not remove token: $token\n";
+        }
+    } else {
+        echo "Token: $token does not exist\n";
+    }
+}
+?>
 </body>
 </html>
