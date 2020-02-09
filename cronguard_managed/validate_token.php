d77a7f546b87adc6302661fa6647b59fb290f6f0
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2020-02-09 20:44:00 +0100

    refactoring the validation - using one script

--- cronguard_managed/validate_token.php
+++ cronguard_managed/validate_token.php
@@ -12,9 +12,29 @@ echo "<div id=\"nav\"></div>";
 ?>
 <div id="content">
 <h2>Validate a Token</h2> 
-<form method="post" action="validatetoken.php">
+<form method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
 Token <input type="text" name="token" /><br /><br />
-<input type="submit" name="Submit" value="Send Token" />
+<input type="submit" name="submit" value="Send Token" />
 </form>
+<br />
+<?php
+if(isset($_POST['submit'])){
+    include "validatetoken.class.php";
+    if (!empty($_POST['token'])){
+        $token = $_POST['token'];
+    } else {
+        echo "No token provided, aborting";
+        exit();
+    }
+    $token_to_check = new ValidateToken($token);
+
+    $bool = $token_to_check->{'check_token'}();
+    if ($bool){
+        echo "Token: $token is valid";
+    } else {
+        echo "Token: $token is not valid";
+    }
+}
+?>
 </body>
 </html>
