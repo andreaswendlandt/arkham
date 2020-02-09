7c90ec9b847f5fc5358a9e40b6df487eca9c11b6
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2020-02-09 21:05:48 +0100

    refactoring the token generating - using one script

--- cronguard_managed/generate_token.php
+++ cronguard_managed/generate_token.php
@@ -12,9 +12,40 @@ echo "<div id=\"nav\"></div>";
 ?>
 <div id="content">
 <h2>Generate a Token</h2> 
-<form method="post" action="generatetoken.php">
+<form method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
 Email address <input type="text" name="email" /><br /><br />
-<input type="submit" name="Submit" value="Send email address" />
+<input type="submit" name="submit" value="Send email address" />
 </form>
+<br />
+<?php
+if(isset($_POST['submit'])){
+    include "generatetoken.class.php";
+    if (!empty($_POST['email'])){
+        $email = $_POST['email'];
+    } else {
+        echo "No email address provided, aborting";
+        exit();
+    }
+    $gen_token = new GenerateToken($email);
+    $bool_double = $gen_token->{'check_mail_doubling'}($email);
+    if ($bool_double) {
+        $bool_val = $gen_token->{'validate_email'}($email);
+        if ($bool_val){
+            $token = $gen_token->{'generate_token'}();
+            $bool_gen = $gen_token->{'write_to_database'}($token, $email);
+                if ($bool_gen){
+                    echo "Email: $email is valid and a new database entry was created\n";
+                    echo "Your Token is: \"$token\"";
+                } else {
+                    echo "Could not create a new database entry, please contact the admin";
+                }
+        } else {
+            echo "Email: $email is not a valid email address\n";
+        }
+    } else {
+        echo "Email: $email already exist!";
+    }
+}
+?>
 </body>
 </html>
