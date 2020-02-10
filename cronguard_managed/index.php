8459c2d25f844068b1cff05a77201c0243503654
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2020-02-10 22:15:40 +0100

    starting with content for the first two sections

--- cronguard_managed/index.php
+++ cronguard_managed/index.php
@@ -12,10 +12,15 @@ echo "<div id=\"nav\"></div>";
 ?>
 <div id="content">
 <h1>Cronguard</h1> 
-<h2>-Let your Cronjobs get supervised-</h2>
+<h2>-Let your Cronjobs get evaluated-</h2>
 
 <p><strong>#What it is</strong></p>
 <p><strong>#How it works</strong></p>
 <p><strong>#What do i need</strong></p>
+Only 2 things, the script cron_wrapper.sh which executes your cronjobs and a valid token <br />
+you can get both on this site, the cron_wrapper.sh script from the download section,<br />
+the token you can generate at the generate token section(all you need for that is a valid email address)<br />
 <p><strong>#What does it cost</strong></p>
+NOTHING, using this service is absolutely and 100% for free! <br />
+If you have a bad conscience about not paying for it - please support your local animal shelter. 
 <p><strong>#About the Author</strong></p>
