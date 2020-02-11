86997b2f4e5aa56a35dd128b6762cabe7ffd9cc4
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2020-02-11 22:07:08 +0100

    adding more content(about me seciton) including some links

--- cronguard_managed/index.php
+++ cronguard_managed/index.php
@@ -12,15 +12,22 @@ echo "<div id=\"nav\"></div>";
 ?>
 <div id="content">
 <h1>Cronguard</h1> 
-<h2>-Let your Cronjobs get evaluated-</h2>
+<h2>- Let your Cronjobs get evaluated -</h2>
 
-<p><strong>#What it is</strong></p>
-<p><strong>#How it works</strong></p>
-<p><strong>#What do i need</strong></p>
+<h3>#What it is</h3>
+<h3>#How it works</h3>
+<h3>#What do i need</h3>
 Only 2 things, the script cron_wrapper.sh which executes your cronjobs and a valid token <br />
 you can get both on this site, the cron_wrapper.sh script from the download section,<br />
 the token you can generate at the generate token section(all you need for that is a valid email address)<br />
-<p><strong>#What does it cost</strong></p>
+<h3>#What does it cost</h3>
 NOTHING, using this service is absolutely and 100% for free! <br />
 If you have a bad conscience about not paying for it - please support your local animal shelter. 
-<p><strong>#About the Author</strong></p>
+<h3>#About the Author</h3>
+My name is andreas, i'm a linux administrator/system engineer and open source enthusiast.<br />
+I live in berlin and worked here for several companies - startups as well as public service and established company composite.<br />
+More about me and what i do on the following links (and about cronguard on github and dockerhub).<br />
+<a href="https://de.linkedin.com/in/andreas-wendlandt-231a3332">LinkedIn</a><br />
+<a href="https://github.com/andreaswendlandt/">GitHub</a><br />
+<a href="https://www.xing.com/profile/Andreas_Wendlandt3">Xing</a><br />
+<a href="https://hub.docker.com/u/andreaswendlandt">Docker Hub</a><br />
