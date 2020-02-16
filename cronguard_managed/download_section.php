b4fa0eb13a1b88ee64edfdb498ac380ecf458448
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2020-02-16 18:20:46 +0100

    adding md5sums to both download files

--- cronguard_managed/download_section.php
+++ cronguard_managed/download_section.php
@@ -14,5 +14,9 @@ echo "<div id=\"nav\"></div>";
 <h1>Download Section</h1> 
 <h3>Download the Cronguard Wrapper</h3> 
 <a href="download_wrapper.php">Download cron_wrapper.sh</a><br />
+<h5>md5sum of cron_wrapper.sh</h5>
+f919be1e227a3727c5ae68e0e714a99a
 <h3>Download the token file</h3> 
 <a href="download_token.php">Download token.inc.sh</a><br />
+<h5>md5sum of token.inc.sh</h5>
+2711b362abf2b680c9efbb1b22936cf9
