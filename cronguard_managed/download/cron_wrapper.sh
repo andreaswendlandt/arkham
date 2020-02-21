16356d541f30b95b5a7779b23e5a52fc6f804f0f
Andreas Wendlandt <andreaswendlandt.79@googlemail.com>
2020-02-21 19:29:32 +0100

    removed useless comments, added the final url, fixed typos

--- cronguard_managed/download/cron_wrapper.sh
+++ cronguard_managed/download/cron_wrapper.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# author: guerillatux
 # desc: wrapper script for cronjobs
+# desc: part of the cronguard lifecycle
 # desc: it notifies the cronguard server via curl about the start-/endtime and the result of whatever it was given to 
-# last modified: 25.01.2020
 
 # Check if something was given to execute
 if [ $# -ne 1 ]; then
@@ -18,8 +17,8 @@ fi
 # Variables
 command="$1"
 host=$(hostname)
-url="http://localhost/cronguard_managed/cronguard.php"
-url_validate_token="http://localhost/cronguard_managed/validatetoken.php"
+url="http://cronguard.ddns.net/cronguard.php"
+url_validate_token="http://cronguard.ddns.net/validate_token_wrapper.php"
 start_time=$(date +%s)
 action="start"
 curl_not_present=0
@@ -50,10 +49,12 @@ if (( $curl_not_present == 0)) && (( $token_not_present == 0 )); then
         token_not_valid=0
     elif [ "$(curl -s -X POST -F "token=$token" $url_validate_token)" == "invalid" ]; then
         echo "Your token \"$token\" is not valid and cronguard will not contacted!"
-	echo "(the same check will be executed on the server side regardless of this one here)"
-	echo "Please generate a new one here: http://url_tba"
+        echo "(the same check will be executed on the server side regardless of this one here)"
+        echo "Please generate a new one here: http://cronguard.ddns.net/generate_token.php"
+        echo "The cronjob will be executed but the cronguard server will not contacted"
     else
-        echo "Could not determine the status of your token \"$token\", please contact mailaddress@tba"
+        echo "Could not determine the status of your token \"$token\", please contact andreas.cronguard@gmail.com"
+        echo "The cronjob will be executed but the cronguard server will not contacted"
     fi
 fi
 
@@ -94,7 +95,7 @@ else
 fi
 
 if check_prerequisites; then
-    # Define the Endtime and make the second Curl, modify the above Database Entry
+    # Define the endtime and make the second curl, modify the above database entry
     action="finished"
     end_time=$(date +%s)
     curl -X POST -F "ident=$ident" -F "token=$token" -F "action=$action" -F "end_time=$end_time" -F "result=$result" $url
