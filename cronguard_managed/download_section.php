6cf47205d3969975f0c298b6b60ed7c0d7a5f21e
Andreas Wendlandt <andreaswendlandt.79@googlemail.com>
2020-02-21 16:52:45 +0100

    fixed wron title

--- cronguard_managed/download_section.php
+++ cronguard_managed/download_section.php
@@ -2,7 +2,7 @@
 <html lang="de"> 
 <head> 
 <meta charset="utf-8"> 
-<title>Cronguard</title> 
+<title>Download Section</title> 
 <meta name="viewport" content="width=device-width; initial-scale=1.0"> 
 </head> 
 <body> 
