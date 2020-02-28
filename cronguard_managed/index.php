f1028dfcc0f8056c113ffb2f35e2ac43264aec49
Andreas Wendlandt <andreaswendlandt.79@googlemail.com>
2020-02-29 00:35:15 +0100

    new menu and new headers

--- cronguard_managed/index.php
+++ cronguard_managed/index.php
@@ -12,13 +12,20 @@
     <meta name="viewport" content="width=device-width; initial-scale=1.0"> 
   </head> 
   <body> 
-  <?php
-  require_once('navigation.php');
-  ?>
+  <div class="ui six item menu">
+    <a class="active item" href="index.php">Home</a>
+    <a class="item" href="generate_token.php">Generate Token</a>
+    <a class="item" href="validate_token.php">Validate Token</a>
+    <a class="item" href="download_section.php">Download Section</a>
+    <a class="item" href="remove_token_mail.php">Remove Token</a>
+    <a class="item" href="forgot_token.php">Forgot Token</a>
+  </div>
   <div class="ui container">
-    <h1 class="ui header">Cronguard</h1> 
-    <h2 class="ui header">- Let your Cronjobs get evaluated -</h2>
-    <h3 class="ui header">#What it is</h3>
+    <div class="ui clearing segment">
+    <h1 class="ui left floated header">Cronguard</h1> 
+    <h3 class="ui right floated header">- Let your Cronjobs get evaluated -</h3>
+    </div>
+    <h4 class="ui top attached inverted header">What it is</h4>
     <div class="ui segment">
       Cronguard is designed to evaluate cronjobs, one might say:"cron can do this this on his own" - yes and no. Yes cron can do that but           it is error-prone, at least for piped commands, consider you execute the following:
       <p>true | false | true && echo "success" || else echo "fail"<br/ >
@@ -29,7 +36,7 @@
       Instead of disabling the mailing from cron let Cronguard do the (dirty and lazy) work. Cronguard will only send mails in case of failed       cronjobs and cronjobs that are running longer than one day. About successfully executed ones Cronguard does not care about.<br />
       (technically for Cronguard every cronjob is an entry in a database and the successful ones will just be deleted)
     </div>
-    <h3 class="ui header">#How it works</h3>
+    <h4 class="ui top attached inverted header">How it works</h4>
     <div class="ui segment">
       You let your cronjob execute by the cron_wrapper.sh script, just put the script in front of your cronjob the following way: <br /><br />
       15  3  *  *  *  /opt/cronguard/cron_wrapper.sh "command" <br />
@@ -42,15 +49,15 @@
       In case you want to manage cronguard on your own you can download everything you need for that(including .deb packages) from the              GitHub link.bEven some minimally invasive testing is possible as there is a docker image on DockerHub:<br />
       <a href="https://hub.docker.com/r/andreaswendlandt/cronguard">Cronguard on Docker Hub</a><br />
     </div>
-    <h3 class="ui header">#What do i need</h3>
+    <h4 class="ui top attached inverted header">What do i need</h4>
     <div class="ui segment">
       Only 2 things, the script cron_wrapper.sh which executes your cronjobs and a valid token. You can get both on this site, the                  cron_wrapper.sh script from the download section, and the token you can generate at the generate token section(all you need for               that is a valid email address)
     </div>
-    <h3 class="ui header">#What does it cost</h3>
+    <h4 class="ui top attached inverted header">What does it cost</h4>
     <div class="ui segment">
       NOTHING, using this service is absolutely and 100% for free! If you have a bad conscience about not paying for it - please support your       local animal shelter. 
     </div>
-    <h3 class="ui header">#About the author</h3>
+    <h4 class="ui top attached inverted header">About the author</h4>
     <div class="ui segment">
       My name is andreas, i'm a linux administrator/system engineer and open source enthusiast. I live in berlin and worked here for several        companies - startups as well as public service and established company composite. More about me and what i do on the following links          (and about cronguard on github and dockerhub).<br />
       <a href="https://de.linkedin.com/in/andreas-wendlandt-231a3332">LinkedIn</a><br />
