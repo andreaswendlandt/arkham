975b91d5ce6bc043bab3b2928a235ab765a75638
Andreas Wendlandt <andreaswendlandt.79@googlemail.com>
2020-03-01 18:04:09 +0100

    new semantic-ui form

--- cronguard_managed/remove_token_mail.php
+++ cronguard_managed/remove_token_mail.php
@@ -1,52 +1,67 @@
 <!DOCTYPE html> 
 <html lang="en"> 
-<head> 
-<meta charset="utf-8"> 
-<title>Remove Token</title> 
-<meta name="viewport" content="width=device-width; initial-scale=1.0"> 
-</head> 
-<body> 
-<?php
-require_once('navigation.php');
-?>
-<div id="content">
-<h2>Remove your Token and Mailaddress</h2>
-Here you can remove your token and your mailaddress by typing your token into the form,<br />
-it will check if the token is a valid one and then remove the corresponding database entry<br />
-(with the token and the mailaddress)<br />
-Keep in mind that there will be no further confirmation - if you press the `Send Token` button<br />
-your token/mailaddress will be deleted.<br />
-If you don't know your token anymore and want your token/mailaddress deleted anyway - go to the<br />
-<a href="http://cronguard.ddns.net/forgot_token.php">Forgot Token</a> Site, let your token send to you<br />
-and delete it afterwards here.<br />
-<br />
-<form method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
-Token <input type="text" name="token" /><br /><br />
-<input type="submit" name="submit" value="Send Token" />
-</form>
-<br />
-<?php
-if(isset($_POST['submit'])){
-    include "class/removetokenmail.class.php";
-    if (!empty($_POST['token'])){
-        $token = $_POST['token'];
-    } else {
-        echo "No token provided, aborting!";
-        exit();
-    }
-    $rem_token = new RemoveTokenMail($token);
-    $token_valid = $rem_token->{'check_token'}();
-    if ($token_valid){
-        $bool = $rem_token->{'remove_from_database'}($token);
-        if ($bool){
-            echo "Token: $token removed\n";
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+    <script
+      src="https://code.jquery.com/jquery-3.1.1.min.js"
+      integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+      crossorigin="anonymous"></script>
+    <script src="semantic/dist/semantic.min.js"></script>
+    <title>Remove Token</title>
+    <meta name="viewport" content="width=device-width; initial-scale=1.0">
+  </head>
+  <body> 
+    <div class="ui six item menu">
+      <a class="item" href="index.php">Home</a>
+      <a class="item" href="generate_token.php">Generate Token</a>
+      <a class="item" href="validate_token.php">Validate Token</a>
+      <a class="item" href="download_section.php">Download Section</a>
+      <a class="active item" href="remove_token_mail.php">Remove Token</a>
+      <a class="item" href="forgot_token.php">Forgot Token</a>
+    </div>
+    <div class="ui container">
+      <div class="ui clearing segment">
+        <h2 class="ui left floated header">Remove your Token and Mailaddress</h2>
+      </div>
+    <h4 class="ui top attached inverted header">What will be done here</h4>
+    <div class="ui segment">
+      Here you can remove your token and your mailaddress by typing your token into the form, it will check if the token is a valid one and         then remove the corresponding database entry (with the token and the mailaddress). Keep in mind that there will be no further                 confirmation - if you press the `Submit` button your token/mailaddress will be deleted. If you don't know your token anymore and want         your token/mailaddress deleted anyway - go to the <a href="https://cronguard.ddns.net/forgot_token.php">Forgot Token</a> Site, let your        token send to you and delete it afterwards here.
+    </div>
+    <form class="ui form" method="POST" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
+      <div class="fields">
+        <div class="eight wide field">
+          <label>Token</label>
+          <input type="text" name="token" placeholder="Token">
+        </div>
+      </div>
+      <button class="ui button" type="submit" name="submit">Submit</button>
+    </form
+    <br />
+    <br />
+    <?php
+    if(isset($_POST['submit'])){
+        include "class/removetokenmail.class.php";
+        if (!empty($_POST['token'])){
+            $token = $_POST['token'];
         } else {
-            echo "Could not remove token: $token\n";
+            echo "Please provide a Token";
+            exit();
         }
-    } else {
-        echo "Token: $token does not exist\n";
+        $rem_token = new RemoveTokenMail($token);
+        $token_valid = $rem_token->{'check_token'}();
+        if ($token_valid){
+            $bool = $rem_token->{'remove_from_database'}($token);
+            if ($bool){
+                echo "Token: $token removed";
+            } else {
+                echo "Could not remove token: $token, please contact the admin";
+            }
+        } else {
+            echo "Token: $token does not exist";
+        }
     }
-}
-?>
-</body>
+    ?>
+    </div>
+  </body>
 </html>
